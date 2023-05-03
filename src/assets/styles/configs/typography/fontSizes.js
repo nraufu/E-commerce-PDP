5d994c0762c8aa65d23359f3331a9c0a7ebb3075
Niyonzi Raufu <nraufu@gmail.com>
2023-05-03 12:00:00 +0200

    add mobile navbar + navbar bugfix :sparkles:

--- src/assets/styles/configs/typography/fontSizes.js
+++ src/assets/styles/configs/typography/fontSizes.js
@@ -1,6 +1,8 @@
 module.exports = {
   title: ['44px', '48px'],
+  'paragraph-big-alt': ['18px', '26px'],
   'paragraph-medium': ['16px', '26px'],
+  'paragraph-small': ['15px', '26px'],
   icon: '20px',
   'subtitle-small': ['13px', 'auto'],
   'paragraph-big': ['28px', 'auto'],
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import clsx from 'clsx';
+import Cart from '../Cart/Cart';
 import NavBarItem from './NavbarItem/NavbarItem';
 import Logo from '../../assets/images/logo.svg';
 import UserIcon from '../../assets/images/image-avatar.png';
-import Cart from '../Cart/Cart';
+import MenuIcon from '../../assets/images/icon-menu.svg';
+import CloseIcon from '../../assets/images/icon-close.svg';
 
 export default function Navbar({ cartItems }) {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const menuRef = useRef(null);
+
   const navItems = [
     { label: 'Collections', link: '/' },
     { label: 'Men', link: '/men' },
@@ -13,12 +20,37 @@ export default function Navbar({ cartItems }) {
     { label: 'Contact', link: '/contact' },
   ];
 
+  const navbarClass = clsx('navbar', {
+    'navbar--open': showMenu,
+  });
+
+  const handleToggleMenuClick = () => setShowMenu(!showMenu);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (showMenu && menuRef.current && !menuRef.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  });
+
   return (
-  <nav className="navbar">
+  <nav className={ navbarClass }>
     <div className="navbar__right-part">
+      <button className='navbar__hamburger-menu' onClick={ handleToggleMenuClick }>
+        <img src={ MenuIcon } alt="closed-menu" className='navbar__hamburger-menu--open'/>
+      </button>
       <img src={ Logo } alt="logo" className="navbar__logo" />
 
-      <ul className="navbar__menu">
+      <ul className="navbar__menu" ref={ menuRef }>
+        <button className='navbar__hamburger-menu' onClick={ handleToggleMenuClick }>
+          <img src={ CloseIcon } alt="opened-menu" className='navbar__hamburger-menu--close' />
+        </button>
         { navItems.map((item, index) => (
           <NavBarItem key={ index } label={ item.label } link={ item.link } />
         )) }
